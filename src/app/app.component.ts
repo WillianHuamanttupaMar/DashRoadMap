7922e43577bcf7f8b826d77734efc1f5bd5521a2
RamirezJosue <xdemobtkx@gmail.com>
2021-08-22 23:41:36 -0500

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,11 +19,54 @@ export class AppComponent implements OnInit {
   // Preguntas e indicadores
   preguntas: any[] = [];
   provideSelect: any[] = [];
+  meses: any[] = [];
 
-  constructor(public jsonService: JsonService) {}
+  constructor(public jsonService: JsonService) { }
 
   ngOnInit() {
     this.getData();
+  }
+
+
+  groupBy(objectArray: any, property: any) {
+    return objectArray.reduce((acc: any, obj: any) => {
+      var key = obj[property];
+      if (!acc[key]) {
+        acc[key] = [];
+      }
+      acc[key].push(obj);
+      return acc;
+    }, {});
+  }
+
+  groupByPercente(array: any) {
+    const resMonth = array.reduce((a: any, data: any) => {
+      a[data.preguntas] = a[data.preguntas] || { pregunta: data.preguntas, valor: 0, contador: 0, porcentaje: 0, mes: data.mes }
+      a[data.preguntas].valor += data.respuesta_valor
+      a[data.preguntas].contador += data.contador
+      a[data.preguntas].porcentaje = (a[data.preguntas].valor * 100) / a[data.preguntas].contador;
+      return a;
+    }, []);
+
+    const resultados: any = [];
+    for (const key in resMonth) {
+      resultados.push(resMonth[key])
+    }
+
+
+    resultados.sort(function (a:any, b:any) {
+      return a[0] - b[0];
+    });
+
+
+    return resultados;
+  }
+
+  groupByTwo(array: any, key: any) {
+    return Array.from(array
+      .reduce((m: any, o: any) => m.set(o[key], [...(m.get(o[key]) || []), o]), new Map)
+      .values()
+    );
   }
 
   getData() {
@@ -31,7 +74,110 @@ export class AppComponent implements OnInit {
       this.camiones = SalidaCamiones;
       this.preguntas = [...new Set(this.camiones.map(c => c.preguntas))];
       this.empresas = [...new Set(this.camiones.map(c => c.empresa))];
+      // const meses = [...new Set(this.camiones.map(c => c.mes))];
+      // console.log(meses);
+
+      // const unique = this.camiones.filter((set => (f:any) => !set.has(f.preguntas) && set.add(f.preguntas))(new Set));
+      // console.log('hola mundo',unique);
+
+      this.camiones = SalidaCamiones.map((temp: any) => ({ ...temp, contador: Number(temp.contador), respuesta_valor: Number(temp.respuesta_valor) }));
+
+      //  console.log(this.camiones);
+
+      // const res = this.camiones.reduce((a, {preguntas, contador}) => (a[preguntas] = (a[preguntas] || 0) + +contador, a), {});
+      // console.log(res)
+
+
+      // const temporal = SalidaCamiones.map((temp: any) => {
+      //   const mes:any = {};
+      //   switch (temp.mes) {
+      //     case 'January':
+      //       console.log('Enero',temp);
+      //       break;
+      //     case 'February':
+      //       console.log('Febrero',temp);
+      //       break;
+      //     case 'March':
+      //       console.log('Marzo',temp);
+      //       break;
+      //     case 'April':
+      //       console.log('Abril',temp);
+      //       break;
+      //     case 'May':
+      //       console.log('Mayo',temp);
+      //       break;
+      //     case 'June':
+      //       console.log('Junio',temp);
+      //       break;
+      //     case 'July':
+      //       console.log('Julio',temp);
+      //       break;
+      //     case 'August':
+      //       console.log('Agosto',temp);
+      //       break;
+      //     case 'September':
+      //       console.log('Setiembre',temp);
+      //       break;
+      //     case 'October':
+      //       console.log('Octubre',temp);
+      //       break;
+      //     case 'November':
+      //       console.log('Noviembre',temp);
+      //       break;
+      //     case 'December':
+      //       console.log('Diciembre',temp);
+      //       break;
+      //     default:
+      //         console.log('hola mudno')
+      //   }
+
+      // });
+
+
+
+      const groupQuestions = this.groupBy(this.camiones, 'mes');
+      // console.log(groupQuestions)
+      // const groupQuestionsTwo = this.groupByTwo(this.camiones, 'preguntas');
+      // console.log(...groupQuestionsTwo)
+
+      const enero = groupQuestions['January'];
+
+      const abril = groupQuestions['April'];
+      const agosto = groupQuestions['August'];
+
+      const abrilResultados = this.groupByPercente(abril);
+      const agostoResultados = this.groupByPercente(agosto);
+      console.log(abrilResultados);
+      // console.log(agostoResultados);
+
+      const oficial = [
+        {
+          name: 'Abril',
+          mes: abrilResultados,
+        },
+        {
+          name: 'Agosto',
+          mes: agostoResultados,
+        }
+      ]
+
+      console.log(oficial)
+
+
+      // console.log(groupQuestions)
+      // const groupMonths = this.groupBy(groupQuestions['1. ¿El conductor cumplió con la inspección física de la unidad y llenado del check list de inspección?'], 'mes');
+      // console.log(groupMonths);
+      // const temporal:any = [];
+
+      // console.log(groupQuestions)
+      // for (const key in groupQuestions) {
+      //   const meses = this.groupBy(groupQuestions[key], 'mes');
+      //   this.preguntas.push({pregunta: key, meses});
+      // }
+
+      // console.log(this.preguntas)
     });
+
   }
 
   /*groupByQuestions( data: any) {
